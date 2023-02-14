cb2eb5a86ed751f39b7c07522d871403d60028d4
ClayClimber <leonardo.zattara@libero.it>
2023-02-14 18:48:58 +0100

    Added interface implementation

--- Chess_GitHub/src/Bishop.java
+++ Chess_GitHub/src/Bishop.java
@@ -1,2 +1,16 @@
-public class Bishop {
+public class Bishop implements ChessPiece{
+    @Override
+    public void move(int x, int y) {
+        
+    }
+
+    @Override
+    public String checkMoves(String[][] board) {
+        return null;
+    }
+
+    @Override
+    public String currentPosition() {
+        return null;
+    }
 }
