e3430bd90a5138e025e8dbf4a16b805d2c542edb
ClayClimber <leonardo.zattara@libero.it>
2023-02-14 23:02:17 +0100

    Added class Constructor

--- Chess_GitHub/src/Rook.java
+++ Chess_GitHub/src/Rook.java
@@ -2,6 +2,11 @@ public class Rook implements ChessPiece{
     private int currentX;
     private int currentY;
 
+    public Rook(){
+        currentX = 0;
+        currentY = 7;
+    }
+
     @Override
     public void move(int x, int y) {
 
