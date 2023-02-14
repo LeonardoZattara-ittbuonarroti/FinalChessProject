be07e0a6145a4c7579ee98aafd9e2e3de57047dc
ClayClimber <leonardo.zattara@libero.it>
2023-02-14 19:13:26 +0100

    Implemented method Move

--- Chess_GitHub/src/King.java
+++ Chess_GitHub/src/King.java
@@ -9,7 +9,10 @@ public class King implements ChessPiece{
 
     @Override
     public void move(int x, int y) {
-
+        if(isLegal(x, y)){
+            currentX = x;
+            currentY = y;
+        }
     }
 
     @Override
