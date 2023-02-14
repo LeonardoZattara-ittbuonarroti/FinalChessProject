978dba12494016170179914c45a89882789d6743
ClayClimber <leonardo.zattara@libero.it>
2023-02-14 19:00:30 +0100

    Added interface implementation

--- Chess_GitHub/src/King.java
+++ Chess_GitHub/src/King.java
@@ -1,3 +1,17 @@
 public class King implements ChessPiece{
-    
+
+    @Override
+    public void move(int x, int y) {
+        
+    }
+
+    @Override
+    public String checkMoves(String[][] board) {
+        return null;
+    }
+
+    @Override
+    public String currentPosition() {
+        return null;
+    }
 }
