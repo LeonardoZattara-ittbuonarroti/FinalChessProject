cb8a766ae8afe3bf6b83e11dd1a85e5fb9efb860
Sami Facchinelli <sami.facchinelli@buonarroti.tn.it>
2023-03-14 23:20:29 +0100

    Added private method IsLegal

--- Chess_GitHub/src/Knight.java
+++ Chess_GitHub/src/Knight.java
@@ -22,6 +22,27 @@ public class Knight implements ChessPiece{
         return null;
     }
 
+    private boolean isLegal(int x, int y){
+        boolean legal = true;
+        if(((x-currentX)*(x-currentX) - (y-currentY)*(y-currentY) != 3) && ((x-currentX)*(x-currentX) - (y-currentY)*(y-currentY) != -3)){
+            legal = false;
+        }
+        if(legal){
+            if((x-currentX) == 0 && (y-currentY) == 0){
+                legal = false;
+            }
+        }
+        if(legal){
+            if(x>7 || x<0){
+                legal = false;
+            }
+            else if(y>7 || y<0){
+                legal = false;
+            }
+        }
+        return legal;
+    }
+
     public int getCurrentX() {
         return currentX;
     }
