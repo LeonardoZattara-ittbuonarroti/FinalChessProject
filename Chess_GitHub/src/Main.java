61f2c1278f2c8547bc4fd5fdbbbf420023ee155d
Sami Facchinelli <sami.facchinelli@buonarroti.tn.it>
2023-03-14 22:59:12 +0100

    Second Pawn Move

--- Chess_GitHub/src/Main.java
+++ Chess_GitHub/src/Main.java
@@ -86,6 +86,26 @@ public class Main {
         }
         System.out.println(out);
         System.out.println("Mossa del pedone eseguita con successo");
+
+        p.move(5, 2);
+        for(int i=0; i<8; i++){
+            for(int j=0; j<8; j++){
+                board[i][j] = "0";
+            }
+        }
+        board[b.getCurrentY()][b.getCurrentX()] = "B";
+        board[r.getCurrentY()][r.getCurrentX()] = "R";
+        board[kg.getCurrentY()][kg.getCurrentX()] = "K";
+        board[p.getCurrentY()][p.getCurrentX()] = "P";
+        out = "";
+        for(int i=0; i<8; i++){
+            out = out+"\n";
+            for(int j=0; j<8; j++){
+                out = out+board[i][j]+"  ";
+            }
+        }
+        System.out.println(out);
+        System.out.println("Mossa del pedone fallita");
     }
 
 }
