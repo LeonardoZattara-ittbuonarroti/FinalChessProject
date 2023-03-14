906ccd5ef20ea1e72ea3e47fa262fd60f89b0faf
Sami Facchinelli <sami.facchinelli@buonarroti.tn.it>
2023-03-14 23:16:33 +0100

    Added interface implementation

--- Chess_GitHub/src/Knight.java
+++ Chess_GitHub/src/Knight.java
@@ -1,2 +1,17 @@
-public class Knight {
+public class Knight implements ChessPiece{
+
+    @Override
+    public void move(int x, int y) {
+        
+    }
+
+    @Override
+    public String checkMoves(String[][] board) {
+        return null;
+    }
+
+    @Override
+    public String currentPosition() {
+        return null;
+    }
 }
